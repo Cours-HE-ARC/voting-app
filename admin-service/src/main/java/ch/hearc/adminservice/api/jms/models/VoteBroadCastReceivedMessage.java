2286ab6f2e3d9fae7dca9d485ff65286fb9925b0
Sébastien Chèvre <seb.chevre@gmail.com>
2024-04-10 15:40:37 +0200

    docker compose nd env vars

--- admin-service/src/main/java/ch/hearc/adminservice/api/jms/models/VoteBroadCastReceivedMessage.java
+++ admin-service/src/main/java/ch/hearc/adminservice/api/jms/models/VoteBroadCastReceivedMessage.java
@@ -1,4 +1,4 @@
-package ch.hearc.adminservice.service.models;
+package ch.hearc.adminservice.api.jms.models;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
